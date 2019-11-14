f6e3b0b71a83f4b7128031d5950dcf5bd0da4434
asifhaider995 <43353121+asifhaider995@users.noreply.github.com>
2019-11-14 14:02:04 +0600

    Test needed

--- GMScrape.py
+++ GMScrape.py
@@ -5,15 +5,17 @@ import csv
 import time
 import datetime
 
-
+# requests data from a single url
 def req_data_from_url(url):
     urllib3.disable_warnings()
-    http = urllib3.PoolManager()
-    req = http.request('GET', url)
+    http = urllib3.PoolManager(num_pools=22)
     success = False
+
     while success is False:
         try:
             # open the url
+            x = len(url)
+            req = http.request('GET', url)
             if (req.status == 200):
                 success = True
         except Exception as e:
@@ -24,29 +26,122 @@ def req_data_from_url(url):
     return req.data
 
 
-def scrape_gm(api, src, dest, freq, duration):
-    # we want to scrape the googlemaps website
+# function to return a list of URLS
+def get_urls(origin, destination, api):
+    # scraping googlemaps website
     site = 'https://maps.googleapis.com/maps/api/'
 
-    # we want to use the distancematrix service
+    # distancematrix service
     service = 'distancematrix/json?'
 
-    # input origin and destination from the user
-    locations = 'origins=' + src + '&destinations=' + dest + '&departure_time=now&'
-
-    # input api key from user
+    # api key
     key = 'key=' + api
 
-    # construct request url
-    request_url = site + service + locations + key
+    # list of urls, combination of origins and destinations
+    url = []
+
+    # form url list
+    for i in origin:
+        for j in destination:
+            if i != j:
+                location = ('origins=' + i + '&destinations=' + j + '&departure_time=now&')
+                url.append(site + service + location + key)
+
+    return url
+
+
+# test function, runs once
+def scrape_gm_test(api, source, destination):
+    # get co-ordinates
+    src = source.values()
+    out = destination.values()
+
+    # get keys
+    in_keys = source.keys()
+    out_keys = destination.keys()
+
+    # fetch request urls
+    request_urls = get_urls(src, out, api)
+
+    # open file
+    # with open('traffic_data.csv', 'w') as file:
+    #   w = csv.writer(file)
+
+    # number of sources
+    src_len = len(src)
+    row = ['timestamp']
+    for i in range(src_len):
+        row.append('travel_time' + in_keys[i] + ' to ' + out_keys[i])
+    #   w.writerow(row)
+    #    step = 1
+    #   while (step <= int(duration * 60 / freq)):
+
+    # number of request urls
+
+    # lists for data, traffic_times, batch
+
+    data = []
+    traffic_times = []
+
+    for i in request_urls:
+        data.append(json.loads(req_data_from_url(i)))
+    for i in data:
+        traffic_times.append(data['rows'][0]['elements'][0]['duration_in_traffic']['text'])
+    batch = [datetime.datetime.now()]
+    for i in traffic_times:
+        batch.append(i)
+    print(batch)
+    #   w.writerow(batch)
+
+    #        if (step % 10 == 0):
+    #           print(str(step) + ' datapoints gathered')
+    #        step += 1
+    #        time.sleep(freq * 60)
+    # data = json.loads(req_data_from_url(request_url))
+    # traffic_time = data['rows'][0]['elements'][0]['duration_in_traffic']['text']
+    # print(str(datetime.datetime.now())+" --- "+str(traffic_time))
+
+
+# Main Scrape_function, runs for bounded time ||| Not yet tested
+def scrape_gm(api, source, destination, freq, duration):
+    # get co-ordinates
+    src = source.values()
+    out = destination.values()
+
+    # get keys
+    in_keys = source.keys()
+    out_keys = destination.keys()
+
+    # fetch request urls
+    request_urls = get_urls(src, out, api)
+
+    # open file
     with open('traffic_data.csv', 'w') as file:
         w = csv.writer(file)
-        w.writerow(['timestamp', 'travel_time'])
+
+        # number of sources
+        src_len = len(src)
+        row = ['timestamp']
+        for i in range(src_len):
+            row.append('travel_time' + in_keys[i] + ' to ' + out_keys[i])
+        w.writerow(row)
         step = 1
         while (step <= int(duration * 60 / freq)):
-            data = json.loads(req_data_from_url(request_url))
-            traffic_time = data['rows'][0]['elements'][0]['duration_in_traffic']['text']
-            batch = (datetime.datetime.now(), traffic_time)
+
+            # number of request urls
+
+            # lists for data, traffic_times, batch
+
+            data = []
+            traffic_times = []
+
+            for i in request_urls:
+                data.append(json.loads(req_data_from_url(i)))
+            for i in data:
+                traffic_times.append(data['rows'][0]['elements'][0]['duration_in_traffic']['text'])
+            batch = [datetime.datetime.now()]
+            for i in traffic_times:
+                batch.append(i)
             w.writerow(batch)
             print(batch)
             if (step % 10 == 0):
@@ -58,13 +153,17 @@ def scrape_gm(api, src, dest, freq, duration):
     # print(str(datetime.datetime.now())+" --- "+str(traffic_time))
 
 
+# Main function | Incomplete code
 def main():
     api_key = input("Enter API key: ")
-    origin = input("Enter Origin co-ordinates: ")
-    destination = input("Enter Origin co-ordinates: ")
-    f = int(input('Enter Frequency in minutes: '))
-    d = int(input('How long to scrape in hours: '))
-    scrape_gm(api_key, origin, destination, f, d)
+    origin = {
+        'Link_Road': '',
+        'Kakoli':''
+    }
+    destination = {}
+    # f = int(input('Enter Frequency in minutes: '))
+    # d = int(input('How long to scrape in hours: '))
+    # scrape_gm(api_key, origin, destination, f, d)
 
 
 if __name__ == '__main__':
