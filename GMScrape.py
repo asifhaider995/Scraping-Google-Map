e47194b8ff5a6df4f677a3c89c303e8360f54c82
asifhaider995 <43353121+asifhaider995@users.noreply.github.com>
2019-11-14 21:46:19 +0600

    Tested, partially complete

--- GMScrape.py
+++ GMScrape.py
@@ -5,6 +5,7 @@ import csv
 import time
 import datetime
 
+
 # requests data from a single url
 def req_data_from_url(url):
     urllib3.disable_warnings()
@@ -22,7 +23,7 @@ def req_data_from_url(url):
             print(e)
             time.sleep(5)
             print("Error for Url " + url + ": " + str(datetime.datetime.now()))
-            print("Retrying")
+            print("Retrying...")
     return req.data
 
 
@@ -50,120 +51,97 @@ def get_urls(origin, destination, api):
     return url
 
 
-# test function, runs once
-def scrape_gm_test(api, source, destination):
-    # get co-ordinates
-    src = source.values()
-    out = destination.values()
 
+# Scrape_function, runs for bounded time
+def scrape_gm(api, source, destination, freq, duration,fname):
+    
+    # get co-ordinates, values
+    src = list(source.values())
+    out = list(destination.values())
+    
     # get keys
-    in_keys = source.keys()
-    out_keys = destination.keys()
-
+    in_keys = list(source.keys())
+    out_keys = list(destination.keys())
+    
     # fetch request urls
     request_urls = get_urls(src, out, api)
 
     # open file
-    # with open('traffic_data.csv', 'w') as file:
-    #   w = csv.writer(file)
-
-    # number of sources
-    src_len = len(src)
-    row = ['timestamp']
-    for i in range(src_len):
-        row.append('travel_time' + in_keys[i] + ' to ' + out_keys[i])
-    #   w.writerow(row)
-    #    step = 1
-    #   while (step <= int(duration * 60 / freq)):
-
-    # number of request urls
-
-    # lists for data, traffic_times, batch
-
-    data = []
-    traffic_times = []
-
-    for i in request_urls:
-        data.append(json.loads(req_data_from_url(i)))
-    for i in data:
-        traffic_times.append(data['rows'][0]['elements'][0]['duration_in_traffic']['text'])
-    batch = [datetime.datetime.now()]
-    for i in traffic_times:
-        batch.append(i)
-    print(batch)
-    #   w.writerow(batch)
-
-    #        if (step % 10 == 0):
-    #           print(str(step) + ' datapoints gathered')
-    #        step += 1
-    #        time.sleep(freq * 60)
-    # data = json.loads(req_data_from_url(request_url))
-    # traffic_time = data['rows'][0]['elements'][0]['duration_in_traffic']['text']
-    # print(str(datetime.datetime.now())+" --- "+str(traffic_time))
-
-
-# Main Scrape_function, runs for bounded time ||| Not yet tested
-def scrape_gm(api, source, destination, freq, duration):
-    # get co-ordinates
-    src = source.values()
-    out = destination.values()
-
-    # get keys
-    in_keys = source.keys()
-    out_keys = destination.keys()
-
-    # fetch request urls
-    request_urls = get_urls(src, out, api)
-
-    # open file
-    with open('traffic_data.csv', 'w') as file:
+    with open(fname, 'w') as file:
         w = csv.writer(file)
 
-        # number of sources
-        src_len = len(src)
+        # number of destinations
+        dest_len = len(out)
         row = ['timestamp']
-        for i in range(src_len):
-            row.append('travel_time' + in_keys[i] + ' to ' + out_keys[i])
+        for i in range(dest_len):
+            row.append('travel_time(in mins): ' + in_keys[0] + ' to ' + out_keys[i])
+
         w.writerow(row)
         step = 1
         while (step <= int(duration * 60 / freq)):
+            start = time.time()
+     
+            # lists for data, traffic_times
 
-            # number of request urls
+            data = list()
+            traffic_times = list()
 
-            # lists for data, traffic_times, batch
-
-            data = []
-            traffic_times = []
-
+            # appends all data unloaded by json in list for all request urls
             for i in request_urls:
                 data.append(json.loads(req_data_from_url(i)))
+
+            # for each data appends all the traffic times in minutes after striping trailing characters
             for i in data:
-                traffic_times.append(data['rows'][0]['elements'][0]['duration_in_traffic']['text'])
+                traffic_times.append((i['rows'][0]['elements'][0]['duration_in_traffic']['text']).strip(' mins'))
+
+            # init batch list with current datetime
             batch = [datetime.datetime.now()]
+
+            # append traffic times in batch list
             for i in traffic_times:
                 batch.append(i)
+
+            # write batch list in rows in .csv
             w.writerow(batch)
             print(batch)
-            if (step % 10 == 0):
-                print(str(step) + ' datapoints gathered')
+
+            #prints steps after gathering 'freq' number of data points
+            if (step % freq == 0):
+                print(str(step) + ' data points gathered')
             step += 1
-            time.sleep(freq * 60)
-    # data = json.loads(req_data_from_url(request_url))
-    # traffic_time = data['rows'][0]['elements'][0]['duration_in_traffic']['text']
-    # print(str(datetime.datetime.now())+" --- "+str(traffic_time))
+
+            end = time.time()
+            elapsed = end - start
+            
+            # let program sleep for freq*60 seconds minus time elapsed to run current step
+            time.sleep((freq * 60) - elapsed)
 
 
-# Main function | Incomplete code
+# Main function
 def main():
-    api_key = input("Enter API key: ")
+    api_key = input('Enter API key: ')
+    filename = (input('Enter name for CSV file: '))+'.csv'
     origin = {
-        'Link_Road': '',
-        'Kakoli':''
+        'Gulshan_1': '23.780622,90.425636',
     }
-    destination = {}
-    # f = int(input('Enter Frequency in minutes: '))
-    # d = int(input('How long to scrape in hours: '))
-    # scrape_gm(api_key, origin, destination, f, d)
+    destination = {
+        'Gulshan_2': '23.794843,90.414243',
+        'Badda': '23.780626,90.425631',
+        'Mohakhali': '23.777927,90.397985',
+        'Hatirjheel': '23.772249,90.414066',
+        'Notun_Bazar': '23.797697,90.423514',
+        'Kakoli': '23.792478,90.400619',
+        'Merul': '23.770742,90.424925',
+        'Tejgaon': '23.761468,90.394344',
+        'JFPark': '23.813985, 90.421195',
+        'ShadhSoroni': '23.790710, 90.388188',
+        'Nikuja_2': '23.837566, 90.418286',
+        'Cantonment': '23.816430,90.405198',
+        'Mirpur': '23.819824, 90.365092'
+    }
+    f = int(input('Enter Frequency in minutes: '))
+    d = float(input('How long to scrape in hours: '))
+    scrape_gm(api_key, origin, destination, f, d,filename)
 
 
 if __name__ == '__main__':
